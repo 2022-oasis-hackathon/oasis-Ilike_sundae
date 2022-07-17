1244416e9648e953bf5d307cb6b1667b1607d12e
minsu <alstn3336@naver.com>
2022-07-18 00:37:58 +0900

    expo 추가

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hihi</Text>
+      <Text>hihi2</Text>
       <StatusBar style="auto" />
     </View>
   );
